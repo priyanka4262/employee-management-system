be3a56e32635e43ce2ef038224b4729ccdb1d4dd
Kumar, Dilip <C-Dilip.Kumar@charter.com>
2021-09-06 21:36:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import EmployeeLogin from "./components/LoginComponent/EmployeeLogin";
 import Homepage from "./components/HomeComponent/Homepage";
 import ForgotPwd from "./components/ForgotPwd/ForgotPwd";
 import ResetPwd from "./components/ResetPwd/ResetPwd";
-import ErrorPage from "./components/ErrorPage/ErrorPage";
 import ChangePwd from "./components/ChangePwd/ChangePwd";
 import Loader from "./components/Loader/Loader";
 
@@ -21,7 +20,6 @@ const App = (props) => {
         <Route exact path="/" component={EmployeeLogin}></Route>
         <Route path="/homepage" component={Homepage}></Route>
         <Route path="/changepwd" component={ChangePwd}></Route>
-        <Route path="/errorpage" component={ErrorPage}></Route>
         <Route exact path="/forgotpwd" component={ForgotPwd}></Route>
         <Route path={"/resetpwd"} component={ResetPwd}></Route>
       </Switch>
